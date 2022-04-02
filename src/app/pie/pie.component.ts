e56037db02f5c3f6d198abbee8a67e74b9f591da
Pierre Cart-Grandjean <pcart-grandjean@amadeus.com>
2022-04-02 13:50:41 +0200

    Install ESLint and resolve issues

--- src/app/pie/pie.component.ts
+++ src/app/pie/pie.component.ts
@@ -1,28 +1,37 @@
 import { Component, OnInit } from '@angular/core';
-import * as d3 from 'd3';
+
+import { arc, pie, PieArcDatum } from 'd3-shape';
+import { scaleOrdinal, ScaleOrdinal } from 'd3-scale';
+import { select, Selection } from 'd3-selection';
+
+interface Framework {
+  Framework: string;
+  Stars: number;
+  Released: number;
+}
 
 // Adopted from Basic pie chart example on D3 Graph Gallery:
 // https://www.d3-graph-gallery.com/graph/pie_basic.html
 @Component({
   selector: 'app-pie',
   templateUrl: './pie.component.html',
-  styleUrls: ['./pie.component.scss']
+  styleUrls: ['./pie.component.scss'],
 })
 export class PieComponent implements OnInit {
-  private data = [
-    {"Framework": "Vue", "Stars": "166443", "Released": "2014"},
-    {"Framework": "React", "Stars": "150793", "Released": "2013"},
-    {"Framework": "Angular", "Stars": "62342", "Released": "2016"},
-    {"Framework": "Backbone", "Stars": "27647", "Released": "2010"},
-    {"Framework": "Ember", "Stars": "21471", "Released": "2011"},
+  private data: Framework[] = [
+    { Framework: 'Vue', Stars: 166443, Released: 2014 },
+    { Framework: 'React', Stars: 150793, Released: 2013 },
+    { Framework: 'Angular', Stars: 62342, Released: 2016 },
+    { Framework: 'Backbone', Stars: 27647, Released: 2010 },
+    { Framework: 'Ember', Stars: 21471, Released: 2011 },
   ];
-  private svg;
+  private svg?: Selection<SVGGElement, Framework, HTMLElement, Framework>;
   private margin = 50;
   private width = 750;
   private height = 600;
   // The radius of the pie chart is half the smallest side
   private radius = Math.min(this.width, this.height) / 2 - this.margin;
-  private colors;
+  private colors?: ScaleOrdinal<number, string, never>;
 
   ngOnInit(): void {
     this.createSvg();
@@ -31,55 +40,56 @@ export class PieComponent implements OnInit {
   }
 
   private createSvg(): void {
-    this.svg = d3.select("figure#pie")
-    .append("svg")
-    .attr("width", this.width)
-    .attr("height", this.height)
-    .append("g")
-    .attr(
-      "transform",
-      "translate(" + this.width / 2 + "," + this.height / 2 + ")"
-    );
+    this.svg = select<SVGGElement, Framework>('figure#pie')
+      .append('svg')
+      .attr('width', this.width)
+      .attr('height', this.height)
+      .append('g')
+      .attr(
+        'transform',
+        'translate(' + this.width / 2 + ',' + this.height / 2 + ')'
+      );
   }
 
   private createColors(): void {
-    this.colors = d3.scaleOrdinal()
-    .domain(this.data.map(d => d.Stars.toString()))
-    .range(["#c7d3ec", "#a5b8db", "#879cc4", "#677795", "#5a6782"]);
+    this.colors = scaleOrdinal<number, string>()
+      .domain(this.data.map((d) => d.Stars))
+      .range(['#c7d3ec', '#a5b8db', '#879cc4', '#677795', '#5a6782']);
   }
 
   private drawChart(): void {
     // Compute the position of each group on the pie:
-    const pie = d3.pie<any>().value((d: any) => Number(d.Stars));
+    const pieGen = pie<any>().value((d: any) => Number(d.Stars));
+
+    // Create the arc generator
+    const arcGen = arc<PieArcDatum<Framework>>()
+      .innerRadius(0)
+      .outerRadius(this.radius);
 
     // Build the pie chart
     this.svg
-    .selectAll('pieces')
-    .data(pie(this.data))
-    .enter()
-    .append('path')
-    .attr('d', d3.arc()
-      .innerRadius(0)
-      .outerRadius(this.radius)
-    )
-    .attr('fill', (d, i) => (this.colors(i)))
-    .attr("stroke", "#121926")
-    .style("stroke-width", "1px");
+      ?.selectAll('pieces')
+      .data(pieGen(this.data))
+      .enter()
+      .append('path')
+      .attr('d', arcGen)
+      .attr('fill', (datum, _) => this.colors!(datum.data.Stars))
+      .attr('stroke', '#121926')
+      .style('stroke-width', '1px');
 
     // Add labels
-    const labelLocation = d3.arc()
-    .innerRadius(100)
-    .outerRadius(this.radius);
+    const labelLocation = arc<PieArcDatum<Framework>>()
+      .innerRadius(100)
+      .outerRadius(this.radius);
 
     this.svg
-    .selectAll('pieces')
-    .data(pie(this.data))
-    .enter()
-    .append('text')
-    .text(d => d.data.Framework)
-    .attr("transform", d => "translate(" + labelLocation.centroid(d) + ")")
-    .style("text-anchor", "middle")
-    .style("font-size", 15);
+      ?.selectAll('pieces')
+      .data(pieGen(this.data))
+      .enter()
+      .append('text')
+      .text((d) => d.data.Framework)
+      .attr('transform', (d) => 'translate(' + labelLocation.centroid(d) + ')')
+      .style('text-anchor', 'middle')
+      .style('font-size', 15);
   }
-
 }
--- src/app/scatter/scatter.component.ts
+++ src/app/scatter/scatter.component.ts
@@ -1,25 +1,39 @@
 import { Component, OnInit } from '@angular/core';
-import * as d3 from 'd3';
+
+import {
+  axisBottom,
+  axisLeft,
+  format,
+  scaleLinear,
+  select,
+  Selection,
+} from 'd3';
+
+interface Framework {
+  Framework: string;
+  Stars: number;
+  Released: number;
+}
 
 // Adopted from Scatterplot example on D3 Graph Gallery:
 // https://www.d3-graph-gallery.com/graph/scatter_tooltip.html
 @Component({
   selector: 'app-scatter',
   templateUrl: './scatter.component.html',
-  styleUrls: ['./scatter.component.scss']
+  styleUrls: ['./scatter.component.scss'],
 })
 export class ScatterComponent implements OnInit {
-  private data = [
-    {"Framework": "Vue", "Stars": "166443", "Released": "2014"},
-    {"Framework": "React", "Stars": "150793", "Released": "2013"},
-    {"Framework": "Angular", "Stars": "62342", "Released": "2016"},
-    {"Framework": "Backbone", "Stars": "27647", "Released": "2010"},
-    {"Framework": "Ember", "Stars": "21471", "Released": "2011"},
+  private data: Framework[] = [
+    { Framework: 'Vue', Stars: 166443, Released: 2014 },
+    { Framework: 'React', Stars: 150793, Released: 2013 },
+    { Framework: 'Angular', Stars: 62342, Released: 2016 },
+    { Framework: 'Backbone', Stars: 27647, Released: 2010 },
+    { Framework: 'Ember', Stars: 21471, Released: 2011 },
   ];
-  private svg;
+  private svg?: Selection<SVGGElement, Framework, HTMLElement, Framework>;
   private margin = 50;
-  private width = 750 - (this.margin * 2);
-  private height = 400 - (this.margin * 2);
+  private width = 750 - this.margin * 2;
+  private height = 400 - this.margin * 2;
 
   ngOnInit(): void {
     this.createSvg();
@@ -27,49 +41,47 @@ export class ScatterComponent implements OnInit {
   }
 
   private createSvg(): void {
-    this.svg = d3.select("figure#scatter")
-    .append("svg")
-    .attr("width", this.width + (this.margin * 2))
-    .attr("height", this.height + (this.margin * 2))
-    .append("g")
-    .attr("transform", "translate(" + this.margin + "," + this.margin + ")");
+    this.svg = select<SVGGElement, Framework>('figure#scatter')
+      .append('svg')
+      .attr('width', this.width + this.margin * 2)
+      .attr('height', this.height + this.margin * 2)
+      .append('g')
+      .attr('transform', 'translate(' + this.margin + ',' + this.margin + ')');
   }
 
   private drawPlot(): void {
     // Add X axis
-    const x = d3.scaleLinear()
-    .domain([2009, 2017])
-    .range([ 0, this.width ]);
-    this.svg.append("g")
-    .attr("transform", "translate(0," + this.height + ")")
-    .call(d3.axisBottom(x).tickFormat(d3.format("d")));
+    const x = scaleLinear().domain([2009, 2017]).range([0, this.width]);
+    this.svg
+      ?.append('g')
+      .attr('transform', 'translate(0,' + this.height + ')')
+      .call(axisBottom(x).tickFormat(format('d')));
 
     // Add Y axis
-    const y = d3.scaleLinear()
-    .domain([0, 200000])
-    .range([ this.height, 0]);
-    this.svg.append("g")
-    .call(d3.axisLeft(y));
+    const y = scaleLinear().domain([0, 200000]).range([this.height, 0]);
+    this.svg?.append('g').call(axisLeft(y));
 
     // Add dots
-    const dots = this.svg.append('g');
+    const dots = this.svg?.append('g');
 
-    dots.selectAll("dot")
-    .data(this.data)
-    .enter()
-    .append("circle")
-    .attr("cx", d => x(d.Released))
-    .attr("cy", d => y(d.Stars))
-    .attr("r", 7)
-    .style("opacity", .5)
-    .style("fill", "#69b3a2");
+    dots
+      ?.selectAll('dot')
+      .data(this.data)
+      .enter()
+      .append('circle')
+      .attr('cx', (d) => x(d.Released))
+      .attr('cy', (d) => y(d.Stars))
+      .attr('r', 7)
+      .style('opacity', 0.5)
+      .style('fill', '#69b3a2');
 
-    dots.selectAll("text")
-    .data(this.data)
-    .enter()
-    .append("text")
-    .text(d => d.Framework)
-    .attr("x", d => x(d.Released))
-    .attr("y", d => y(d.Stars))
+    dots
+      ?.selectAll('text')
+      .data(this.data)
+      .enter()
+      .append('text')
+      .text((d) => d.Framework)
+      .attr('x', (d) => x(d.Released))
+      .attr('y', (d) => y(d.Stars));
   }
 }
